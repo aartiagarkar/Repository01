6d37c9e320596272e49d38fce820dace6fe19f09
pranitpatil01 <pranitpatil01@gmail.com>
2018-12-06 13:38:06 +0530

    commite

--- src/com/scp/java/Demo1.java
+++ src/com/scp/java/Demo1.java
@@ -4,7 +4,8 @@ public class Demo1 {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("project aala");
-		System.out.println("panya nalayak");
+		
+		System.out.println("panya mand");
 	}
 
 }
