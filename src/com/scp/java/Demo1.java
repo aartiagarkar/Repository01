e8be7836a9cc4ac48c8fbacf8c0346026387af41
swapnaliDesai <swapnalidesai29@gmail.com>
2018-12-06 13:25:58 +0530

    hiiiii

--- src/com/scp/java/Demo1.java
+++ src/com/scp/java/Demo1.java
@@ -4,6 +4,7 @@ public class Demo1 {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("project aala");
+		System.out.println("panya nalayak");
 	}
 
 }
