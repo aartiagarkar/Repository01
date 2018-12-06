a2d4ff832cb9c7d0a2f700165177b335703d88ab
pranitpatil01 <pranitpatil01@gmail.com>
2018-12-06 13:41:12 +0530

    swapu

--- src/com/scp/java/Demo1.java
+++ src/com/scp/java/Demo1.java
@@ -4,7 +4,7 @@ public class Demo1 {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("project aala");
-		
+		System.out.println("swapu nalayak");
 		System.out.println("panya mand");
 	}
 
