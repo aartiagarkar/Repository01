1adeb9e85ca439c126e52d5edfb068d9d6fad31c
BhaktiSP <bhaktipotdar903@gmail.com>
2018-12-15 17:39:47 +0530

    Change

--- src/com/scp/java/Demo1.java
+++ src/com/scp/java/Demo1.java
@@ -2,6 +2,7 @@ package com.scp.java;
 
 public class Demo1 {
 	public static void main(String[] args) {
+		System.out.println("");
 		System.out.println("Hello world");
 		System.out.println("project aala");
 		System.out.println("swapu nalayak");
